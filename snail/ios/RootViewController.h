df0cc8eb2e19d0fb28a2fac51ef5dd5e122396d7
icanmakeudance <icanmakeudance@gmail.com>
2013-09-04 00:02:42 +0800

    first

---
+++ snail/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  snailAppController.h
+//  snail
+//
+//  Created by 王 文义 on 13-9-3.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
---
+++ snail/ios/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  snail
+//
+//  Created by 王 文义 on 13-9-3.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
